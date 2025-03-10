bc97d0dfc53613ff7119909c79a751c590b9b083
mrunal712 <132483570+mrunal712@users.noreply.github.com>
2025-03-10 21:32:47 +0530

    Solution for Valid Parentheses.

---
+++ date_03_10_2025/20.ValidParentheses.java
@@ -0,0 +1,27 @@
+//20. Valid Parentheses
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(') {
+                    return false;
+                } else if (c == '}' && top != '{') {
+                    return false;
+                } else if (c == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+      return stack.isEmpty();
+    }
+}
