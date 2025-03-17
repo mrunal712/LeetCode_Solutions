d4458d5366a999ba03f1c33fdbaca7298d62a2db
mrunal712 <132483570+mrunal712@users.noreply.github.com>
2025-03-17 22:53:24 +0530

    Solution for Contains Duplicate.

---
+++ date_03_13_2025/217.ContainsDuplicate.java
@@ -0,0 +1,19 @@
+//217. Contains Duplicate
+ 
+import java.util.HashSet;
+ 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num : nums) 
+        {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+ 
