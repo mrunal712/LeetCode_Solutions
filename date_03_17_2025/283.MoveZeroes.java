31e2a6ee76ba559b47026eb1d971158a7b6022e0
mrunal712 <132483570+mrunal712@users.noreply.github.com>
2025-03-17 23:44:14 +0530

    Solution for Move Zeroes.

---
+++ date_03_17_2025/283.MoveZeroes.java
@@ -0,0 +1,26 @@
+//283. Move Zeroes
+
+public class Solution {
+    public static int[] moveZeroes(int[] nums) {
+        int j = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if (j == -1) return nums;
+
+        for (int i = j + 1; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+        return nums;
+    }
+}
