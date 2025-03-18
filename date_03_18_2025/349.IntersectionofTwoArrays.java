b965403d635da9ad400ee721036e556ad45cae52
mrunal712 <132483570+mrunal712@users.noreply.github.com>
2025-03-18 14:12:56 +0530

    Solution for Intersection of Two Arrays.

---
+++ date_03_18_2025/349.IntersectionofTwoArrays.java
@@ -0,0 +1,31 @@
+//349. Intersection of Two Arrays
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1=new HashSet<>();
+
+        for(int num:nums1)
+        {
+            set1.add(num);
+        }
+
+        Set<Integer> set2=new HashSet<>();
+
+        for(int num:nums2)
+        {
+            set2.add(num);
+        }
+
+        set1.retainAll(set2);
+
+        int[] unique=new int[set1.size()];
+        int index=0;
+
+        for(int num:set1)
+        {
+            unique[index++]=num;
+        }
+
+      return unique;  
+    }
+}
