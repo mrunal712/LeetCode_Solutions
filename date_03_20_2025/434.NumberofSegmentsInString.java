2aebfc000dbe4db309ee2cdd9511645306af106f
mrunal712 <132483570+mrunal712@users.noreply.github.com>
2025-03-21 00:20:16 +0530

    Solution for Number of Segments in a String.

---
+++ date_03_20_2025/434.NumberofSegmentsInString.java
@@ -0,0 +1,17 @@
+//434. Number of Segments in a String
+
+class Solution {
+    public int countSegments(String s) {
+    
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if((i==0 || s.charAt(i-1)==' ')&& (s.charAt(i)!=' '))
+            {
+                count++;
+            }
+        }
+        
+     return count;
+    }
+}
