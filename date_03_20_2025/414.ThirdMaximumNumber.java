0503a1d129dc917f7e8e2a9d178e3b2aa0e4f031
mrunal712 <132483570+mrunal712@users.noreply.github.com>
2025-03-21 00:04:03 +0530

    Solution for Third Maximum Number.

---
+++ date_03_20_2025/414.ThirdMaximumNumber.java
@@ -0,0 +1,19 @@
+//414. Third Maximum Number
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+        
+        for (int num : nums) {
+            set.add(num); 
+            if (set.size() > 3) {
+                set.pollFirst(); 
+            }
+        }
+        
+        if (set.size() == 3) {
+            return set.first();
+        }
+        return set.last();
+    }
+}
