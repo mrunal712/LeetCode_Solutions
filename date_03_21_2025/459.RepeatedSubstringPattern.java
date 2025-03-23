9fe898d36d4ac7707719dcb04db9f8e927029841
mrunal712 <132483570+mrunal712@users.noreply.github.com>
2025-03-23 21:57:59 +0530

    Solution for Repeated Substring Pattern.

---
+++ date_03_21_2025/459.RepeatedSubstringPattern.java
@@ -0,0 +1,16 @@
+//459. Repeated Substring Pattern
+
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) { 
+                String sub = s.substring(0, i);
+                if (s.equals(sub.repeat(n /i))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
