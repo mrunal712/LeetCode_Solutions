76e1dfb108071e4c52598b364ab293f37725ae97
mrunal712 <132483570+mrunal712@users.noreply.github.com>
2025-03-26 00:15:31 +0530

    Solution for Climbing Stairs.

---
+++ date_03_25_2025/70.ClimbingStairs.java
@@ -0,0 +1,18 @@
+//70. Climbing Stairs
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int first = 1, second = 2, ways = 0;
+
+        for (int i = 3; i <= n; i++) {
+            ways = first + second;
+            first = second;
+            second = ways;
+        }
+
+        return second;
+    }
+}
